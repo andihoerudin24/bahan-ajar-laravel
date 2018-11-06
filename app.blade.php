6107dafc42cf0882f6f4d1797dad88454bb183e8
AndiHoerudin <andihoerudin24@gmail.com>
2018-11-05 20:35:06 -0800

    Update app.blade.php

--- app.blade.php
+++ app.blade.php
@@ -52,6 +52,9 @@
                                 @endif
                             </li>
                         @else
+                         <li class="nav-item">
+                                <a class="nav-link" href="#">Kategori</a>
+                        </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
